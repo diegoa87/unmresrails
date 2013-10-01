d2ccdb1efd36f89e78d249e93fb5be13419d84fc
Diego Arias <diagom27@gmail.com>
2013-10-01 10:51:43 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Unmesrails::Application.config.secret_token = 'cf1ffb3662b36db1dafd6b34fcd73fe0aefbbb1a2698ab2a8f57dd10b4eecded1443000211741192ae1e8581b9a1f2c116297ba3c9784f7e5a6dd2b99e42e6cd'
